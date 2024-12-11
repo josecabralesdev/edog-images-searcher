45d2896df02130f4d754d95888a6c6406fb9013f
joseac <josecabralesdev@gmail.com>
2024-12-11 12:32:25 -0500

    404 issue fixed

--- src/hooks/useFetchRandomImages.js
+++ src/hooks/useFetchRandomImages.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from 'react'
 
 const useFetchRandomImages = (breed, imagesAmount) => {
-  const [data, setData] = useState({})
+  const [data, setData] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
   useEffect(() => {
     fetch(`https://dog.ceo/api/breed/${breed}/images/random/${imagesAmount}`)
       .then(res => res.json())
-      .then(data => setData(data))
-      .catch(err => setError(err))
+      .then(data => {
+        if (data.status === 'success') {
+          setData(data.message)
+          setError(null)
+        }
+        if (data.status === 'error') {
+          setData([])
+          setError(data.message)
+        }
+      })
+      .catch(error => setError(error))
       .finally(() => setLoading(false))
   }, [breed, imagesAmount])
 
